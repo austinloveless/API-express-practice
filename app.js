10d2c8ed37037218d9373ee00c64346c3ca4b656
austinloveless <austinloveless5171@gmail.com>
2018-07-12 15:35:13 -0600

    express routing with an api, uses ejs templating and request package for api call

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
 const request = require("request");
+const port = process.env.PORT || 3000;
+const parse = require("body-parser");
 
-app.get("/results", (req, res) => {
-  res.send("hello");
+app.set("view engine", "ejs");
+app.use(parse.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("search");
 });
 
-request(
-  "http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb",
-  function(error, response, body) {
-    console.log("error:", error); // Print the error if one occurred
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-    console.log("body:", body); // Print the HTML for the Google homepage.
-  }
-);
-
+app.get("/results", (req, res) => {
+  let search = req.query.search;
+  request(`http://www.omdbapi.com/?s=${search}&apikey=thewdb`, function(
+    error,
+    response,
+    body
+  ) {
+    if (!error && response.statusCode == 200) {
+      console.log("error:", error); // Print the error if one occurred
+      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+      console.log("body:", body); // Print the HTML for the Google homepage.
+      const data = JSON.parse(body);
+      res.render("results", { data: data });
+    }
+  });
+});
 app.listen(port, () => {
   console.log("listening on port", port);
 });
